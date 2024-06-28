aad025f670408712d957efa224f2f1bbe4955cc9
梁典典 <413153189@qq.com>
2024-06-28 15:39:05 +0800

    细节优化

--- src/components/foot.tsx
+++ src/components/foot.tsx
@@ -8,20 +8,20 @@ const Foot: React.FC = () => {
                 <p>Copyright © 2024 - All right reserved by 典典博客 Ltd</p>
                 <p className={'flex flex-wrap items-center justify-center gap-4 mb-5'}>
                     <a href={"https://github.com/mdddj"} rel={'noreferrer'} target={"_blank"}
-                       className={'link my-foot-link'}>
+                    >
                         Github
                     </a>
-                    <a href={"https://admin.itbug.shop"} rel={'noreferrer'} target={"_blank"}
-                       className={'link my-foot-link'}>
+                    <a href={"https://admin.itbug.shop"} rel={'noreferrer'} target={"_blank"}>
                         管理后台
                     </a>
-                    <a className={'link my-foot-link'}
-                       href={'https://apifox.com/apidoc/shared-6f74775d-40ca-4a07-ad1e-dd9c8480f927'} rel={'noreferrer'}
-                       target={"_blank"}>开放API接口</a>
+                    <a
+                        href={'https://apifox.com/apidoc/shared-6f74775d-40ca-4a07-ad1e-dd9c8480f927'}
+                        rel={'noreferrer'}
+                        target={"_blank"}>开放API接口</a>
 
                     <span>
-                        博客源码 (<a className={'link'} href={'https://github.com/mdddj/blog'}>前端</a>)(<a
-                        href={'https://github.com/mdddj/dd_server'} className={'link'}>后端</a>)
+                        博客源码 (<a href={'https://github.com/mdddj/blog'}>前端</a>)(<a
+                        href={'https://github.com/mdddj/dd_server'}>后端</a>)
                     </span>
 
 
@@ -30,7 +30,7 @@ const Foot: React.FC = () => {
                                                                                   className={'w-4 h-4 object-cover'}
                                                                                   alt={'备案'}/>粤公网安备44011302004470</a>
                     <a href={'https://beian.miit.gov.cn/#/Integrated/recordQuery'} target={'_blank'}
-                       className={"my-foot-link link flex gap-0.5 items-center"}>赣ICP备17011549号-1</a>
+                       className={""}>赣ICP备17011549号-1</a>
                 </p>
             </div>
 
